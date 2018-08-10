7c1ddb7fc9e851da2bf95d7bc3ae4f054714d239
sugihiro1 <sugiyama@k00.itscom.net>
2018-08-10 11:39:50 +0000

    add validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,6 +44,10 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);
+        
         $task = new Task;
         $task->content = $request->content;
         $task->status = $request->status;
@@ -93,6 +97,10 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required|max:10',
+        ]);        
+        
         $task = Task::find($id);
         $task->content = $request->content;
         $task->status = $request->status;
